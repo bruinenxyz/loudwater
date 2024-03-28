4c7d80c6243a002438677b0059585d2691f78205
Grant Pierce <glpierce174@gmail.com>
2024-03-28 16:04:58 -0700

    Implement initial relations creation UI

---
+++ frontend/src/data/use-relations.ts
@@ -0,0 +1,80 @@
+import useSWR, { useSWRConfig } from "swr";
+import {
+  backendCreate,
+  backendDelete,
+  backendGet,
+  backendUpdate,
+} from "./client";
+import useSWRMutation from "swr/mutation";
+import * as _ from "lodash";
+
+export function useRelations(databaseId?: string) {
+  const { mutate } = useSWRConfig();
+
+  const { data, isLoading, isValidating, error } = useSWR<Relation[]>(
+    databaseId ? `/relations/db/${databaseId}` : null,
+    backendGet,
+  );
+
+  // Update the individual relation caches with the data returned from the server
+  _.forEach(data, (relation: Relation) => {
+    mutate(`/relations/${relation.id}`, relation);
+  });
+
+  return { data, isLoading, isValidating, error };
+}
+
+export const useRelation = (id?: string) => {
+  const { data, error, isLoading, isValidating, mutate } = useSWR<Relation>(
+    id ? `/relations/${id}` : null,
+    backendGet,
+  );
+
+  return { data, error, isLoading, isValidating, mutate };
+};
+
+export const useCreateRelation = () => {
+  const { mutate } = useSWRConfig();
+  const { data, error, trigger, isMutating } = useSWRMutation(
+    "/relations",
+    async (
+      url: string,
+      { arg }: { arg: CreateRelation },
+    ): Promise<Relation> => {
+      return await backendCreate("/relations", arg);
+    },
+  );
+  return { data, error, trigger, isMutating };
+};
+
+export const useUpdateRelation = () => {
+  const { mutate } = useSWRConfig();
+  const { data, error, trigger, isMutating } = useSWRMutation(
+    `/relations`,
+    async (
+      url: string,
+      { arg }: { arg: { id: string; update: UpdateRelation } },
+    ) => {
+      const updateRelationResponse = await backendUpdate(
+        `${url}/${arg.id}`,
+        arg.update,
+      );
+
+      mutate(`/relations/${arg.id}`, updateRelationResponse, false);
+      mutate(`/tables/db/${updateRelationResponse.database_id}`);
+
+      return updateRelationResponse;
+    },
+  );
+
+  return { data, error, trigger, isMutating };
+};
+
+export const useDeleteRelation = (id?: string) => {
+  const { data, error, trigger, isMutating } = useSWRMutation(
+    `/relations/${id}`,
+    (url: string) => backendDelete(url),
+  );
+
+  return { data, error, trigger, isMutating };
+};
