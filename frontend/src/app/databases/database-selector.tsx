0e555a7a8d738f234c9cb7bbaf05af2e5483f044
Grant Pierce <glpierce174@gmail.com>
2024-04-09 10:55:45 -0700

    Update navigation bar to only show tables in selected schema

--- frontend/src/app/databases/database-selector.tsx
+++ frontend/src/app/databases/database-selector.tsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { H6, Menu, MenuDivider, MenuItem, Text } from "@blueprintjs/core";
-import { useSelectedDatabase } from "@/stores";
+import { MenuDivider, MenuItem, Text } from "@blueprintjs/core";
 import {
   useDatabaseSchemas,
   useDatabases,
   useUpdateDatabase,
 } from "@/data/use-database";
 import AddDatabase from "@/app/databases/add-database";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-const DatabaseSelector = ({ selectedDatabase, setSelectedDatabase }: any) => {
+
+const DatabaseSelector = ({
+  selectedDatabase,
+  setSelectedDatabase,
+  selectedSchema,
+  setSelectedSchema,
+}: any) => {
   const {
     data: databases,
     isLoading: isLoadingDatabases,
@@ -22,14 +27,9 @@ const DatabaseSelector = ({ selectedDatabase, setSelectedDatabase }: any) => {
     useUpdateDatabase(selectedDatabase?.id);
 
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedSchema, setSelectedSchema] = useState(
-    selectedDatabase?.schema,
-  );
 
   useEffect(() => {
-    if (selectedDatabase) {
-      setSelectedSchema(selectedDatabase.schema);
-    }
+    setSelectedSchema(undefined);
   }, [selectedDatabase]);
 
   const handleUpdateDatabaseSchema = async (schema: string) => {
